520261b3dcbb12034ef379e626cafc4fba67e5e1
Drago6777 <edward.firespinner@gmail.com>
2017-04-18 15:00:07 +0100

    Update Create.sql

--- src/main/java/structures/Create.sql
+++ src/main/java/structures/Create.sql
@@ -1,28 +1,28 @@
 CREATE TABLE login
 (
-    userID INT(5) NOT NULL,
-    username CHAR(56) NOT NULL,
+    userID INT(5) NOT NULL AUTO_INCREMENT,
+    username VARCHAR(56) NOT NULL,
     password VARCHAR(256) NOT NULL,
     userType int(1) NOT NULL,
     PRIMARY KEY(userID)
-)
+);
 
 CREATE TABLE words
 (
-    wordID INT(5) NOT NULL,
+    wordID INT(5) NOT NULL AUTO_INCREMENT,
     english VARCHAR(56) NOT NULL,
     welsh VARCHAR(56) NOT NULL,
     wordType char(1) NOT NULL,
     wordGender char(1) NOT NULL,
     PRIMARY KEY(wordID)
-)
+);
 
 CREATE TABLE results
 (
-    testID int(5) NOT NULL,
+    testID int(5) NOT NULL AUTO_INCREMENT,
     username CHAR(56) NOT NULL,
     restult int(2) NOT NULL,
     outOf int(2) NOT NULL,
     PRIMARY KEY(testID),
     FOREIGN KEY(username) REFERENCES login(username)
-)
+);
